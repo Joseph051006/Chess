7d80eae573819e5889237bb96645017fc4fecab7
Joseph <167524188+Joseph051006@users.noreply.github.com>
2025-08-28 17:05:20 +0200

    Added some text

--- Chess/src/Board.java
+++ Chess/src/Board.java
@@ -1,6 +1,18 @@
-public class Board {
+import java.util.Arrays;
 
+public  class Board {
+
+    public String[][]board = new String[8][8];
     public void setUpBoard(){
+
+       for (int r = 0; r < 8; r++ ){
+           for (int c = 0; c < 8; c++){
+               this.board[c][r] = "[]";
+           }
+           this.board[7][r] = "[]\n";
+
+       }
+        System.out.println(Arrays.deepToString(board));
 
     }
 
